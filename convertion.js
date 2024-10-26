425d1082a603f05ea92a45eda5a17a6a6500b321
Avinashpalofficial <apal73451@gmail.com>
2024-10-26 13:50:42 +0530

    rev

---
+++ convertion.js
@@ -0,0 +1,15 @@
+ const num = null
+ const val= 1
+// console.log(typeof num);
+ const valNum= Number(val)
+ console.log(num);
+ let valInNumber = Boolean(val)
+// console.log(valInNumber);
+ //console.log(typeof true);
+ //console.log(valNum);
+ 
+console.log(valInNumber);
+
+
+ 
+ 
---
+++ text.js
@@ -0,0 +1,10 @@
+// console.log("kavya");
+
+const accountId= 12345
+let bankName = "SBI"
+var emailId= "apl12345@gmail.com"
+console.table([accountId,bankName,emailId])
+//accountId =12;  //this is not allowed hum const ko ek bar hi assign kr skte  hai 
+bankName = "punjab"
+emailId = "apld@djf"
+console.table([accountId,bankName,emailId])
