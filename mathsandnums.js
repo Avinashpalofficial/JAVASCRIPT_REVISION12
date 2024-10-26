4afbd411225953b1fdc293743cfa6955128bea32
Avinashpalofficial <apal73451@gmail.com>
2024-10-27 00:10:56 +0530

    maths

---
+++ mathsandnums.js
@@ -0,0 +1,37 @@
+const num = 100
+// console.log(num);
+
+const num1 = new Number(100)
+// console.log(num1);
+
+// console.log(num1.toString()); 
+// console.log(typeof num1.toString());
+// console.log(num1.toString().length);
+
+const score = 123.445
+// console.log(score.toPrecision(4));
+
+const balance = 100000
+// console.log(balance.toLocaleString('en-IN'));
+
+//  ***************maths*****************
+
+const value = -4
+// console.log(Math.abs(value));
+const val2= 4.99
+// console.log(Math.round(val2));
+// console.log(Math.ceil(val2))
+ const val3 = 6.32
+// console.log(Math.floor(val3));
+// console.log(Math.floor(Math.random(val3)*10));
+
+
+const min = 20
+const max = 30
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
+
+
