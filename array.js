43e3014910c159229ebdb068eb8105b881f988c8
Avinashpalofficial <apal73451@gmail.com>
2024-10-29 12:27:35 +0530

    array

---
+++ array.js
@@ -0,0 +1,42 @@
+const myArray = [12,3,4,5,2,7]
+//console.log(myArray[0]);
+
+//myArray.push([2])
+//console.log(myArray);
+//console.log(myArray[4]);
+
+
+//console.log(myArray.unshift(1));
+
+// console.log(myArray);
+
+// console.log(myArray.shift());
+// console.log((myArray.includes(23)));
+
+// console.log(myArray);
+
+// console.log(myArray.indexOf(4));
+// console.log(myArray);
+
+// const newarr = myArray.join()
+// console.log(newarr);
+// console.log(typeof newarr);
+
+
+const myarr1 = myArray.slice(1,4)
+
+console.log( "A",myArray);
+console.log(myarr1);
+
+const myarr2 = myArray.splice(2,5)
+
+console.log("B",myArray);
+console.log(myarr2);
+
+
+
+
+
+
+
+
