b40ae9293e3546d9a3091b45a57fa468c6a5f471
Avinashpalofficial <apal73451@gmail.com>
2024-10-29 13:14:54 +0530

    01

---
+++ 01_array.js
@@ -0,0 +1,38 @@
+const player = ["Dhoni", "vIRAT", "RAINA", "YUVI"];
+const badmintonPlayer = ["PIVI", "RAHUL", "GAUTAM"];
+
+//  player.push(badmintonPlayer)
+
+//const newTeam = player
+
+// console.log(newTeam);
+
+const updatePlayer = player.concat(badmintonPlayer);
+
+// const updatePlayer = player
+
+//console.log(updatePlayer);
+
+// spreadoperator
+
+const newUpdatePlayer = [...player, ...badmintonPlayer];
+
+// console.log(newUpdatePlayer);
+
+const myArray = Array.isArray(["avin"]) 
+// console.log(myArray);
+
+const  my_Array = Array.from("AVINASH")
+console.log( my_Array);
+
+
+  
+
+let  player1 = "avi"
+let  player2 =  "rah"
+let  player3 = "sag"
+
+
+// console.log(Array.of(player1,player2,player3));
+
+
