15c2ab691d280a4b5487da405e7f067bac979e4f
Avinashpalofficial <apal73451@gmail.com>
2024-10-30 22:24:00 +0530

    destruct

---
+++ destruct.js
@@ -0,0 +1,17 @@
+const my_Course= {
+    courseName : "jsLearning",
+    price : "Free",
+    courseInstructor:"Avinash"
+}
+  console.log(my_Course.courseName);
+
+//   using destructring method
+
+  const {courseInstructor :abcid} =my_Course
+  //console.log(courseInstructor);
+  console.log(abcid);
+  
+
+  
+  
+  
