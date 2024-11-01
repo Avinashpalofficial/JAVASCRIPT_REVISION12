b79918f0b5416f0454ed4542e6e6da9692be39cf
Avinashpalofficial <apal73451@gmail.com>
2024-11-01 14:09:01 +0530

    iifefunction in js

---
+++ iife.js
@@ -0,0 +1,16 @@
+(function chai(){
+    console.log("connected");
+    
+
+}) ();
+
+ ((name)=>{
+    console.log(`Connected two ${name}`);
+
+    ((user)=>{
+        console.log(`Connected two ${user}`);
+        
+    }) ("sam")
+    
+ }) ("Avinash")
+// chai()
