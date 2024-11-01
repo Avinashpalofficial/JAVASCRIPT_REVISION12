90b0afcb27b0e774d3f2044e109984bfbec70e76
Avinashpalofficial <apal73451@gmail.com>
2024-11-01 09:46:18 +0530

    scopes

---
+++ scope.js
@@ -0,0 +1,21 @@
+function one(user) {
+  user = "Avin";
+  //   return user;
+
+  function two(cart) {
+    cart = "badminton";
+    // console.log(user);
+    // console.log(cart);
+
+    function three() {
+      Price = 449;
+      console.log(user);
+      console.log(cart);
+      console.log(Price);
+    }
+    three();
+  }
+  two();
+  console.log(user);
+}
+one();
