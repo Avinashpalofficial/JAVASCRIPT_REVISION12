3a280acadaeb58bc19df7396b96495a593dfe599
Avinashpalofficial <apal73451@gmail.com>
2024-11-04 11:11:53 +0530

    ternary in js

---
+++ ternary.js
@@ -0,0 +1,12 @@
+const Name = "suraj"
+   Name=="avin"  ? console.log("ys") : console.log("NO");
+   
+   
+
+const icePrice = 100
+
+// icePrice>70  ? console.log("ys") : console.log("No");
+
+const My_name = "avin"
+
+My_name=="avin" ? console.log("ys")  : console.log("No");
